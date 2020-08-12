6b41ed4404eed4b92e5301482f1f6fd3108278d1
ChenTao <chentao1@snqu.com>
2020-08-12 13:05:26 +0800

    给锦鲤鱼添加动画

--- AndroidUI/app/src/main/java/ct/com/ui/MainActivity.kt
+++ AndroidUI/app/src/main/java/ct/com/ui/MainActivity.kt
@@ -9,6 +9,7 @@ import ct.com.ui.course02.SkinActivity
 import ct.com.ui.course04.Course04Activity
 import ct.com.ui.course05.Course05Activity
 import ct.com.ui.course07.Course07Activity
+import ct.com.ui.course08.Course08Activity
 import ct.com.ui.course12.Course12Activity
 import ct.com.ui.course15.Course15Activity
 import kotlin.reflect.KClass
@@ -27,6 +28,7 @@ class MainActivity : AppCompatActivity() {
             R.id.btn_course04 -> readyGo(Course04Activity::class)
             R.id.btn_course05 -> readyGo(Course05Activity::class)
             R.id.btn_course07 -> readyGo(Course07Activity::class)
+            R.id.btn_course08 -> readyGo(Course08Activity::class)
             R.id.btn_course12 -> readyGo(Course12Activity::class)
             R.id.btn_course13 -> readyGo(Course05Activity::class)
             R.id.btn_course14 -> readyGo(Course05Activity::class)
---
+++ AndroidUI/app/src/main/java/ct/com/ui/course08/Course08Activity.kt
@@ -0,0 +1,15 @@
+package ct.com.ui.course08
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ct.com.ui.R
+import kotlinx.android.synthetic.main.activity_course08.*
+
+class Course08Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course08)
+
+        img_course08_fish.setImageDrawable(FishDrawable02())
+    }
+}
